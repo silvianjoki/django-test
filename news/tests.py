0bb413c35a3295fe6bac718f45d2e61af9c795c4
silvianjoki <10silvianjoki@gmail.com>
2022-03-27 15:36:12 +0300

    readd bootsrap 5

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
+
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))
